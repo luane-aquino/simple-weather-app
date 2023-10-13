272f18030cd45776686226fefe6b749fb725f178
Guilherme Nass <guilherme.nass@zup.com.br>
2023-10-13 16:57:18 -0300

    refatoração para evitar várias renderizações e separação de responsabilidade de funções

--- script.js
+++ script.js
@@ -1,110 +1,89 @@
-import KEY from "./config.js";
+import fetchWeather from './api/weather.js';
 
-var formEl = document.getElementById("searchForm");
-var cityEl = document.getElementById("city");
-let cardsEl = document.getElementById("cards");
-let errorMsgEl = document.getElementById("error-msg");
+let searchFormElement = document.getElementById("searchForm");
+let cityElement = document.getElementById("city");
+let cardsElement = document.getElementById("cards");
+let errorMessageElement = document.getElementById("error-msg");
 let cardsList = [];
 
-formEl.addEventListener("submit", (event) => {
-  event.preventDefault();
-  submitForm();
-  clearField();
-});
-
-function submitForm() {
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityEl.value}&appid=${KEY}&units=metric`;
-  fetch(url)
-    .then((response) => {
-      if (response.status === 200) {
-        return response.json();
-      } else {
-        throw new Error("please search for a valid city");
-      }
-    })
-    .then((response) => {
-      if (cityAlreadyExist(response)) {
-        throw new Error(
-          `${response.name} ${response.sys.country} already exist`,
-        );
-      }
-      hideErrorMessage();
-      updateCardsList(response);
-      showWeatherInfo();
-    })
-    .catch((error) => {
-      console.error("msg:", error);
-      const errorText = error.toString().replace("Error: ", "");
-      showErrorMessage(errorText);
-    });
+const RESPONSE_STATUS_TEXT = {
+  "404": "please search for a valid city",
+  "500": "an internal error occurred. Try again!"
 }
 
-function cityAlreadyExist(city) {
-  return cardsList.find(
-    (item) => item.name === city.name && item.sys.country === city.sys.country,
-  );
+searchFormElement.addEventListener("submit", (event) => {
+  event.preventDefault();
+  submitForm();
+  clearInputValue();
+});
+
+function clearInputValue() {
+  cityElement.value = "";
+}
+
+function getErrorMessage(statusCode) {
+  return RESPONSE_STATUS_TEXT[statusCode] || ""
+}
+
+async function submitForm() {
+  const data = await fetchWeather(cityElement.value)
+  const response = await data.json()
+  const statusCode = response.cod
+  if (statusCode >= 400) {
+    const errorMessage = getErrorMessage(statusCode)
+    showErrorMessage(errorMessage)
+    return
+  }
+  hideErrorMessage();
+  updateCardsList(response);
 }
 
 function showErrorMessage(text) {
-  errorMsgEl.innerText = text;
-  errorMsgEl.classList.add("visible");
-  errorMsgEl.setAttribute("aria-hidden", "false");
+  errorMessageElement.innerText = text;
+  errorMessageElement.classList.add("visible");
+  errorMessageElement.setAttribute("aria-hidden", "false");
 }
 
 function hideErrorMessage() {
-  if (errorMsgEl.classList.contains("visible")) {
-    errorMsgEl.classList.remove("visible");
-    errorMsgEl.setAttribute("aria-hidden", "true");
+  if (errorMessageElement.classList.contains("visible")) {
+    errorMessageElement.classList.remove("visible");
+    errorMessageElement.setAttribute("aria-hidden", "true");
   }
 }
 
-function updateCardsList(info) {
-  let isNewItem = true;
-
-  const newList = cardsList.map((item) => {
-    if (item.name === info.name && item.sys.country === info.sys.country) {
-      isNewItem = false;
-      return info;
-    }
-    return item;
-  });
-
-  cardsList = newList;
-  isNewItem && cardsList.push(info);
+function cityAlreadyExists(weatherData) {
+  return cardsList.some(cardItem => cardItem.name === weatherData.name && cardItem.sys.country === weatherData.sys.country)
 }
 
-function showWeatherInfo() {
-  let html = "";
+function updateCardsList(weatherData) {
+  if (!cityAlreadyExists(weatherData)) {
+    cardsList.push(weatherData);
+    renderWeatherInfo(weatherData)
+  }
+}
 
-  for (let i = 0; i < cardsList.length; i++) {
-    const iconUrl = `https://openweathermap.org/img/wn/${cardsList[i].weather[0].icon}@2x.png`;
-    const iconDesc = `https://openweathermap.org/img/wn/${cardsList[i].weather[0].description}@2x.png`;
-
-    html += `
+function renderWeatherInfo(response) {
+  let html = cardsElement.innerHTML;
+  const iconUrl = `https://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png`;
+  const iconAlt = `https://openweathermap.org/img/wn/${response.weather[0].description}@2x.png`;
+  html += `
     <li class='card'>
-      <div class='card__city-country-wrapper' aria-label='${
-        cardsList[i].name
-      }, ${cardsList[i].sys.country}'>
-        <h3 class='card__title' aria-hidden>${cardsList[i].name}</h3>
-        <span class='card__badge' aria-hidden>${cardsList[i].sys.country}</span>
+      <div class='card__city-country-wrapper' aria-label='${response.name
+    }, ${response.sys.country}'>
+        <h3 class='card__title' aria-hidden>${response.name}</h3>
+        <span class='card__badge' aria-hidden>${response.sys.country}</span>
       </div>
       <div class='card__temperature-wrapper' aria-label='${Math.round(
-        cardsList[i].main.temp,
-      )} °C'>
+      response.main.temp,
+    )} °C'>
         <p class='temperature-wrapper__number' aria-hidden>${Math.round(
-          cardsList[i].main.temp,
-        )}</p>
+      response.main.temp,
+    )}</p>
         <span class='temperature-wrapper__symbol' aria-hidden>°C</span>
       </div>
-      <img src='${iconUrl}' alt='${iconDesc}' width='80' height='80'/>
-      <p class='card__weather-desc'>${cardsList[i].weather[0].description}</p>
+      <img src='${iconUrl}' alt='${iconAlt}' width='80' height='80'/>
+      <p class='card__weather-desc'>${response.weather[0].description}</p>
     </li>
    `;
-  }
-
-  cardsEl.innerHTML = html;
-}
-
-function clearField() {
-  cityEl.value = "";
+  cardsElement.innerHTML = html;
 }
