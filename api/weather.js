86c27c3f857a07aa9ecff83ed18696c6393ce9b8
Luane Aquino <luane.aquino.br@gmail.com>
2023-10-24 07:12:23 +0900

    added key

--- api/weather.js
+++ api/weather.js
@@ -1,6 +1,6 @@
-import { APP_KEY } from "../config.js";
-
 export default function fetchWeather(city) {
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APP_KEY}&units=metric`;
+  // in a real application this key need to be in a .env file
+  const KEY = "bd60ee3a03cda093bd7daefd75613eb1";
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${KEY}&units=metric`;
   return fetch(url);
 }
